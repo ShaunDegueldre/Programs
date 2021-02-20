277222c02fe6dde2d60e89d494a6fb4afec37564
Shaun Degueldre <shaun.degueldre@snhu.edu>
2021-02-20 15:03:11 -0500

    Add Programs

--- ClassBasicPublic/song.cpp
+++ ClassBasicPublic/song.cpp
@@ -1,9 +1,11 @@
 #include "song.hpp"
 
 // add Song method definitions here:
+//function type class name method name method type
 void Song::add_title(std::string new_title) {
 	title = new_title;
 }
+//data type class name method name
 std::string Song::get_title() {
 	return title;
 }
--- ClassBasicPublic/song.hpp
+++ ClassBasicPublic/song.hpp
@@ -2,10 +2,14 @@
 #include <string>
 
 // add the Song class here:
+//class name
 class Song {
+	variable within class
 	std::string title;
 
+//access specifier
 public:
+	//function within class
 	void add_title(std::string new_title);
 	std::string get_title();
 };
