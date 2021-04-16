bc43661d0bbde12fce547f961f088406a4236e56
Shaun Degueldre <shaun.degueldre@snhu.edu>
2021-04-16 14:50:41 -0400

    Add Programs

---
+++ C#/MethodAsArguments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlternateExpressions
+{
+    class Program
+    {
+        // Method to be used as second argument
+        // Method which is determining if a word is longer than 8 characters
+        public static bool IsLong(string word)
+        {
+            return word.Length > 8;
+        }
+
+        static void Main(string[] args)
+        {
+            // Array to be used as first argument
+            // Array of words that describe 
+            string[] adjectives = { "rocky", "mountainous", "cosmic", "extraterrestrial" };
+
+            // Call Array.Find(). This is a method that takes an array and a method as an argument.  Returns the first element for which the method returns true 
+            // Pass in the array and method as arguments
+            string firstLongAdjective = Array.Find(adjectives, IsLong);
+
+            Console.WriteLine($"The first long word is: {firstLongAdjective}.");
+        }
+    }
+}
---
+++ C#/MethodLambdaExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlternateExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] spaceRocks = { "meteoroid", "meteor", "meteorite" };
+
+            // bool makesContact = Array.Exists(spaceRocks, HitGround);
+            // below changes the above so that the method of HitGround is defined within the makesContact method
+            // this is called a lambda expression
+            bool makesContact = Array.Exists(spaceRocks, (string s) => s == "meteorite");
+
+            // this method can also be written further shorthand.  Removing the type for the second parameter and the parentheses 
+            bool makesContact = Array.Exists(spaceRocks, s => s == "meteorite");
+
+            if (makesContact)
+            {
+                Console.WriteLine("At least one space rock has reached the Earth's surface!");
+            }
+        }
+
+        static bool HitGround(string s)
+        {
+            return s == "meteorite";
+        }
+    }
+}
