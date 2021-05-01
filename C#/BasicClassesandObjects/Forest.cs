b6f46b99be5133508da0e52df26b9268ffca9511
Shaun Degueldre <shaun.degueldre@snhu.edu>
2021-04-30 23:03:53 -0400

    Add Programs

---
+++ C#/BasicClassesandObjects/Forest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    // creation of the class Forst
+    class Forest
+    {
+        // Adding four fields to the forest class
+        public string name;
+        public int trees;
+        public int age;
+        public string biome;
+
+    }
+
+}
+
---
+++ C#/BasicClassesandObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // providing values to the fields within the forest class
+            Forest f = new Forest();
+            f.name = "Crazy";
+            f.age = 400;
+            f.trees = 10000;
+            f.biome = "FULL";
+
+            // Printing to console the name of the created f forest class
+            Console.WriteLine(f.name);
+        }
+    }
+}
