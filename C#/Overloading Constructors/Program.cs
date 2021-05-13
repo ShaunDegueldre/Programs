c8200efb65866328ab027effeb2de87659da59cd
Shaun Degueldre <shaun.degueldre@snhu.edu>
2021-05-13 11:59:22 -0400

    Add Programs

---
+++ C#/Overloading Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest("Congo", "Tropical");
+            f.Trees = 0;
+
+            Console.WriteLine(f.Name);
+            Console.WriteLine(f.Biome);
+            // Input for constructor that takes only one argument
+            Forest f1 = new Forest("Rendlesham");
+            // Printing to console the Name and Biome for the constructor used with only one arguement
+            Console.WriteLine(f1.Biome);
+            Console.WriteLine(f1.Name);
+        }
+    }
+}
